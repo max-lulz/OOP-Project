611f2dea48178665b9ceeb82cceb62c9f94e3fa7
Aryesh <aryeshkoya27@gmail.com>
2020-03-30 18:58:50 +0530

    Add login verification

--- app/src/main/java/com/example/mrdelivery/LoginActivity.java
+++ app/src/main/java/com/example/mrdelivery/LoginActivity.java
@@ -1,14 +1,87 @@
 package com.example.mrdelivery;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Objects;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText inputUserName, inputPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        inputUserName = findViewById(R.id.login_email_input);
+        inputPassword = findViewById(R.id.login_password_input);
+        Button login = findViewById(R.id.login_btn);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                authenticateUser();
+            }
+        });
     }
+
+    private void authenticateUser(){
+        final String userName = inputUserName.getText().toString();
+        final String password = inputPassword.getText().toString();
+
+        boolean fieldsNotFilled = (TextUtils.isEmpty(userName) || TextUtils.isEmpty(password));
+
+        if(fieldsNotFilled)
+        {
+            Toast.makeText(this,"Fill all fields",Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            final DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference();
+
+            rootRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    if(dataSnapshot.child("Users").child(userName).exists())
+                    {
+                        if(Objects.requireNonNull(dataSnapshot.child("Users").child(userName).child("Password").getValue()).toString().equals(password))
+                        {
+                            Toast.makeText(LoginActivity.this,"Login Successful!",Toast.LENGTH_SHORT).show();
+                        }
+                        else
+                        {
+                            Toast.makeText(LoginActivity.this,"Incorrect Password",Toast.LENGTH_SHORT).show();
+                        }
+                        Log.d("pass", Objects.requireNonNull(dataSnapshot.child("Users").child(userName).child("Password").getValue().toString()));
+                    }
+                    else
+                    {
+                        Toast.makeText(LoginActivity.this,"No user with given User Name found",Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                }
+            });
+        }
+    }
+
+
 }
--- app/src/main/java/com/example/mrdelivery/RegisterActivity.java
+++ app/src/main/java/com/example/mrdelivery/RegisterActivity.java
@@ -2,18 +2,14 @@ package com.example.mrdelivery;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.AppCompatRadioButton;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
 import android.widget.Switch;
 import android.widget.Toast;
 
@@ -24,15 +20,14 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.rey.material.widget.CheckBox;
 
 import java.util.HashMap;
 
 public class RegisterActivity extends AppCompatActivity {
 
     private EditText InputName,InputEmail,InputPassword,InputMobileNumber,InputConfirmPassword;
-    private Switch deliveryperson;
-    private ProgressDialog loadingbar;
+    private Switch deliveryPerson;
+    private ProgressDialog loadingBar;
 
 
     @Override
@@ -40,19 +35,16 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        Button createaccount = (Button) findViewById(R.id.register_btn);
-        InputName=(EditText) findViewById(R.id.register_name_input);
-        InputEmail=(EditText)findViewById(R.id.register_email_input);
-        InputMobileNumber=(EditText)findViewById(R.id.register_mobilenumber_input);
-        InputConfirmPassword=(EditText)findViewById(R.id.register_confirmpassword_input);
-        InputPassword=(EditText)findViewById(R.id.register_password_input);
-        loadingbar=new ProgressDialog(this);
-        deliveryperson=(Switch) findViewById(R.id.delivery_rad);
+        Button createAccount = findViewById(R.id.register_btn);
+        InputName = findViewById(R.id.register_name_input);
+        InputEmail = findViewById(R.id.register_email_input);
+        InputMobileNumber = findViewById(R.id.register_mobilenumber_input);
+        InputConfirmPassword = findViewById(R.id.register_confirmpassword_input);
+        InputPassword = findViewById(R.id.register_password_input);
+        loadingBar = new ProgressDialog(this);
+        deliveryPerson = findViewById(R.id.delivery_rad);
 
-
-
-
-        createaccount.setOnClickListener(new View.OnClickListener() {
+        createAccount.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v)
             {
@@ -60,69 +52,68 @@ public class RegisterActivity extends AppCompatActivity {
             }
         });
     }
-        private void CreateAccount()
+    private void CreateAccount()
+    {
+        String name = InputName.getText().toString();
+        String email = InputEmail.getText().toString();
+        String password = InputPassword.getText().toString();
+        String confirmPassword = InputConfirmPassword.getText().toString();
+        String mobileNumber = InputMobileNumber.getText().toString();
+        boolean deliveryCheck = deliveryPerson.isChecked();
+
+        boolean fieldsNotFilled = (TextUtils.isEmpty(name) || TextUtils.isEmpty(email) ||
+                TextUtils.isEmpty(password) || TextUtils.isEmpty(mobileNumber) ||
+                TextUtils.isEmpty(confirmPassword));
+
+        if(fieldsNotFilled)
         {
-            int count=0;
-            String name = InputName.getText().toString();
-            String email = InputEmail.getText().toString();
-            String password = InputPassword.getText().toString();
-            String confirmPassword = InputConfirmPassword.getText().toString();
-            String mobileNumber = InputMobileNumber.getText().toString();
-            boolean deliveryCheck = deliveryperson.isChecked();
-
-            boolean fieldsNotFilled = (TextUtils.isEmpty(name) || TextUtils.isEmpty(email) ||
-                    TextUtils.isEmpty(password) || TextUtils.isEmpty(mobileNumber) ||
-                    TextUtils.isEmpty(confirmPassword));
-
-            if(fieldsNotFilled)
-            {
-                Toast.makeText(this,"All fields are mandatory",Toast.LENGTH_SHORT).show();
-            }
-            else if(!password.equals(confirmPassword))
-            {
-                Toast.makeText(this,"Passwords don't match",Toast.LENGTH_SHORT).show();
-            }
-            else
-            {
-                loadingbar.setTitle("Create Account");
-                loadingbar.setMessage("Please Wait while we create your account...");
-                loadingbar.setCanceledOnTouchOutside(false);
-                loadingbar.show();
-                ValidateUser(name, email, password, mobileNumber, deliveryCheck);
-            }
+            Toast.makeText(this,"All fields are mandatory",Toast.LENGTH_SHORT).show();
         }
+        else if(!password.equals(confirmPassword))
+        {
+            Toast.makeText(this,"Passwords don't match",Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            loadingBar.setTitle("Create Account");
+            loadingBar.setMessage("Please Wait while we create your account...");
+            loadingBar.setCanceledOnTouchOutside(false);
+            loadingBar.show();
+            ValidateUser(name, email, password, mobileNumber, deliveryCheck);
+        }
+    }
 
     private void ValidateUser(final String name, final String email, final String password, final String mobileNumber, final boolean deliveryCheck)
     {
-        final DatabaseReference rootref;
-        rootref= FirebaseDatabase.getInstance().getReference();
-        rootref.addListenerForSingleValueEvent(new ValueEventListener() {
+        final DatabaseReference rootRef;
+        rootRef= FirebaseDatabase.getInstance().getReference();
+        rootRef.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot)
             {
                 if(!(dataSnapshot.child("Users").child(email).exists()))
                 {
-                    HashMap<String,Object> userdatamap =new HashMap<>();
-                    userdatamap.put("Name",name);
-                    userdatamap.put("Username",email);
-                    userdatamap.put("Mobile Number",mobileNumber);
-                    userdatamap.put("Password",password);
-                    userdatamap.put("DeliverPerson", deliveryCheck);
-                    rootref.child("Users").child(email).updateChildren(userdatamap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    HashMap<String,Object> userDataMap =new HashMap<>();
+                    userDataMap.put("Name",name);
+                    userDataMap.put("Username",email);
+                    userDataMap.put("Mobile Number",mobileNumber);
+                    userDataMap.put("Password",password);
+                    userDataMap.put("DeliverPerson", deliveryCheck);
+                    rootRef.child("Users").child(email).updateChildren(userDataMap).addOnCompleteListener(new OnCompleteListener<Void>() {
                         @Override
                         public void onComplete(@NonNull Task<Void> task)
                         {
                             if(task.isSuccessful())
                             {
                                 Toast.makeText(RegisterActivity.this,"Congratulations Your Account Has Been Created",Toast.LENGTH_SHORT).show();
-                                loadingbar.dismiss();
+                                loadingBar.dismiss();
                                 Intent intent=new Intent(RegisterActivity.this,LoginActivity.class);
                                 startActivity(intent);
                             }
                             else
                                 {
                                     Toast.makeText(RegisterActivity.this,"An Error Occurred Please Retry",Toast.LENGTH_SHORT).show();
-                                    loadingbar.dismiss();
+                                    loadingBar.dismiss();
 
                                 }
                         }
@@ -130,8 +121,7 @@ public class RegisterActivity extends AppCompatActivity {
                 }
                 else
                 {
-
-                    loadingbar.dismiss();
+                    loadingBar.dismiss();
                     Toast.makeText(RegisterActivity.this,"An Account Already Exists With This Username! Try with a different username",Toast.LENGTH_LONG).show();
                 }
             }
